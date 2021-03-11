0b3b37579c36d7e22d805f25d2adea24f9062fe4
Jinny Jang <jangjinny21@gmail.com>
2021-03-11 14:16:08 -0800

    create user tables

---
+++ express-back-end/src/db/schema/01_users.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR (255) NOT NULL,
+  phone_number CHAR(10) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  pin VARCHAR(4) NOT NULL
+);
