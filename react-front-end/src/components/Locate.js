1526ee6e85b7a554b8f7f6f6417cccc9a90f66a1
Jinny Jang <jangjinny21@gmail.com>
2021-03-13 03:55:55 +0000

    refactor Locate component

---
+++ react-front-end/src/components/Locate.js
@@ -0,0 +1,22 @@
+import React from "react";
+import useStyles from './Styles';
+import { Button } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Locate({panTo}) {
+  const classes = useStyles();
+
+  return <Button
+    className={classes.locateButton}
+    variant="contained"
+    startIcon={<SearchIcon />}
+    onClick={() => {
+    navigator.geolocation.getCurrentPosition((position) => panTo({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }), () => null)
+  }}>Find Me!
+  </Button>
+};
+
+export default Locate;
