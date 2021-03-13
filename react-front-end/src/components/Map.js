3dc625be844b0c966ee0a1f835f12ce3bd886ac0
Jinny Jang <jangjinny21@gmail.com>
2021-03-13 00:56:40 +0000

    delete files

---
+++ react-front-end/src/components/Map.js
@@ -0,0 +1,96 @@
+import React, { useState, useCallback, useRef } from 'react'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
+import { formatRelative } from "date-fns";
+
+const containerStyle = {
+  width: '800px',
+  height: '800px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+const libraries = ["places"];
+
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+}
+
+const Map = () => {
+    const { isLoaded, loadError } = useJsApiLoader ({
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+      libraries,
+    })
+    const [markers, setMarkers] = useState([]);
+    const [selected, setSelected] = useState(null);
+  
+    const onMapClick = useCallback((event) => {
+        setMarkers(prev => [
+          ...prev, {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date()
+          }
+        ])
+      }, []);
+  
+    const mapRef = useRef();
+    const onMapLoad = useCallback((map) => {
+      mapRef.current = map;
+    }, [])
+  
+    const panTo = useCallback(({ lat, lng }) => {
+      mapRef.current.panTo({ lat, lng });
+      mapRef.current.setZoom(14);
+    },[]);
+  
+    if (loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading Maps";
+    
+  
+    return (
+      <div>
+      <h1> Safety App <span>🤴🏼</span></h1>
+      <Locate panTo={panTo} />
+      
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={18}
+          options={options}
+          onClick={onMapClick}
+          onLoad={onMapLoad}
+        >
+          {markers.map((marker => (
+          <Marker 
+            key={marker.time.toISOString()}
+            position={{lat: marker.lat, lng: marker.lng}}
+            onClick = { () => { setSelected(marker) }}
+          
+            />)))}
+  
+          {selected ? (<InfoWindow position={{lat: selected.lat, lng: selected.lng}} onCloseClick={() => {setSelected(null)}}>
+            <div>
+              <h2>Crime Reported❗️</h2>
+              <p>Spotted { formatRelative(selected.time, new Date()) }</p>
+            </div>
+          </InfoWindow>) : null}
+        </GoogleMap>
+            
+          </div>
+    )
+  
+    function Locate({panTo}) {
+      return <button  onClick={() => {
+        navigator.geolocation.getCurrentPosition((position) => panTo({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }), () => null)
+      }}>Me</button>
+    }
+};
+
+export default Map;
---
+++ react-front-end/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ForumIcon from '@material-ui/icons/Forum';
+import HomeIcon from '@material-ui/icons/Home';
+import RoomIcon from '@material-ui/icons/Room';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+const Navbar = ( { setSelected, selected } ) => {
+  
+  return (
+    <div>
+  <BottomNavigation
+        value={selected}
+        onChange={(event, newValue) => {
+          setSelected(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Map" icon={<RoomIcon />} />
+        <BottomNavigationAction label="Forum" icon={<ForumIcon />} />
+        <BottomNavigationAction label="Profile" icon={<PersonPinIcon />} />
+      </BottomNavigation>
+      
+    </div>
+  );
+};
+
+export default Navbar;
