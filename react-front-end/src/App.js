481c06f74ca665af452a9aa021c5c33ab0a04563
Jinny Jang <jangjinny21@gmail.com>
2021-03-13 18:10:05 +0000

    add profile and forum page

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Map from './components/Map'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Map from './components/Map';
+import Forum from './components/Forum';
+import Profile from './components/Profile'
 
 const App = () => {
   const [selected, setSelected] = useState(0);
@@ -10,7 +12,10 @@ const App = () => {
   <div>
   <Navbar setSelected={setSelected} selected={selected} />
   
-  {selected === 1 ? (<Map />) : (<Home />)}
+  {selected === 0 ? (<Home />) : null}
+  {selected === 1 ? (<Map />): null}
+  {selected === 2 ? (<Forum />) : null}
+  {selected === 3 ? (<Profile />) : null}
 
   </div>
   );
