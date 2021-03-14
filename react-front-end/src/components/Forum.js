4be4dc250acdc0fdd2d1cca217044a4db1bbac16
Jinny Jang <jangjinny21@gmail.com>
2021-03-14 10:19:14 +0000

    refactor Forum component

--- react-front-end/src/components/Forum.js
+++ react-front-end/src/components/Forum.js
@@ -1,48 +1,92 @@
 import React, { useState } from 'react';
-import CreatePost from './CreatePosts';
-import ShowPost from './ShowPost';
-import { Grid, Fab } from '@material-ui/core';
+import { Box, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import CreatePost from './CreatePost';
 import postStyles from './PostStyles';
+import Post from './Post';
 
-const savedPosts = {
-  title: "Theft at Restaurant",
-  name: "Monica Geller",
-  location: "Vancouver",
-  description: "Someone stole something"
-}
+const incident = [
+  {
+    title: "Theft at Restaurant",
+    user: "Monica Geller",
+    location: "Vancouver",
+    description: "Someone stole something"
+  },
+  {
+    title: "Assault by Comic Book Store",
+    user: "Ross Geller",
+    location: "New York",
+    description: "Pheobe mugged me!"
+  },
+  {
+    title: "Purse Stolen",
+    user: "Rachel Green",
+    location: "New York",
+    description: "Tall man in black hoodie stole my bag!"
+  }
+]
 
 const Forum = () => {
-  const [ selected, setSelected ] = useState(false);
   const classes = postStyles();
+  const [ allPosts, setAllPosts ] = useState(incident);
+  const [ selected, setSelected ] = useState(false);
 
-  return (  
-  <div>
-    <Grid container justify="center" direction="row">
-    <Grid item>
-    <Grid
-          container
-          direction="column"
-          justify="center"
-          spacing={2}
-          className={classes.forum}
-          >
-      <Grid item>
-        <ShowPost title={savedPosts.title} name={savedPosts.name} location={savedPosts.location} description={savedPosts.description}/>
-      </Grid>
-      <Grid item>
-        <Fab onClick={() => setSelected(true)} color="primary" aria-label="add">
-          <AddIcon />
-        </Fab>
-      </Grid>
-      </Grid>
-      </Grid>
-    </Grid>
+  const [ title, setTitle ] = useState("");
+  const [ user, setUser ] = useState("");
+  const [ location, setLocation ] = useState("");
+  const [ description, setDescription ] = useState("");
+  
+  const addPost = () => {
+    const postData = {
+      title,
+      user,
+      location,
+      description
+    };
+  
+    setAllPosts((prev) => {
+      return [...prev, postData]
+    })
+    
+  };
 
-    { selected ? <CreatePost /> : null }
+  return (
+    <div>
+      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Box>
+          <h1>Forum</h1>
+          </Box>
+          <Box margin="30px">
+            <Fab onClick={() => setSelected(true) } color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </Box>
+        </Box>
 
-  </div>
-  );
-};
+        <Box display="flex">
+          <Box margin="50px">
+          { allPosts.map(post => {
+            return (
+              <Box className={classes.postBox}>
+              <div key={post.title+post.user+post.location} >
+                <Post 
+                  title={post.title}
+                  user={post.user}
+                  location={post.location}
+                  description={post.description} />
+              </div>
+              </Box>
+            )})}
+          </Box>
+          
+          <Box margin="50px">
+            { selected ? <CreatePost setTitle={setTitle} setUser={setUser} setLocation={setLocation} setDescription={setDescription}  addPost={addPost} /> : null}
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  )
+}
 
 export default Forum;
