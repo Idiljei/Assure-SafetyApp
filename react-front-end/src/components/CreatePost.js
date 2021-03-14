05b5cd1d32da584fe7a8ac2c5508aa26ad626bfa
Jinny Jang <jangjinny21@gmail.com>
2021-03-14 10:18:08 +0000

    create component for new post

---
+++ react-front-end/src/components/CreatePost.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Button, TextField, Grid, Paper } from '@material-ui/core';
+import postStyles from './PostStyles';
+
+const CreatePost = (props) => {
+  const classes = postStyles();
+  
+  return (
+    <section>
+      <Grid container justify="center" direction="row">
+        <Grid item>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          className={classes.createPost}
+          >
+        <Paper
+          variant="elevation"
+          elevation={3}
+          className={classes.background}
+          >
+          <Grid item>
+          <form noValidate autoComplete="off">
+            <Grid container direction="column">
+              <Grid item>
+                <h2>Report a Crime</h2>
+              </Grid>
+            <Grid item>
+            <TextField 
+              id="standard-secondary" 
+              label="Title" 
+              color="primary" 
+              variant="outlined"
+              onChange={(e) => props.setTitle(e.target.value)} 
+              />
+              </Grid>
+              <Grid item>
+              <TextField 
+                id="standard-secondary" 
+                label="Name" 
+                color="primary" 
+                variant="outlined" 
+                onChange={(e) => props.setUser(e.target.value)} 
+                />
+                </Grid>
+                <Grid item>
+                <TextField 
+                  id="standard-secondary" 
+                  label="Location"
+                  color="primary"
+                  variant="outlined"
+                  onChange={(e) => props.setLocation(e.target.value)} 
+                  />
+                  </Grid>
+                  <Grid item>
+                  <TextField 
+                    id="standard-secondary"
+                    label="Description"
+                    color="primary" 
+                    variant="outlined"
+                    onChange={(e) => props.setDescription(e.target.value)} 
+                    />
+                    </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+              <Grid item>
+              <Button
+                className={classes.submitButton}
+                type="submit"
+                size="large" 
+                variant="contained"
+                onClick={props.addPost} 
+                > Submit
+              </Button>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </section>
+  );
+};
+
+export default CreatePost;
