de4ab9bf9a19d70ff04adaa21258fc95303e6ce1
Jinny Jang <jangjinny21@gmail.com>
2021-03-14 01:36:47 +0000

    style profile page

---
+++ react-front-end/src/components/PostStyles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const postStyles = makeStyles({ 
+  createPost: {
+    justifyContent: "center",
+    minHeight: "80vh"
+  },
+  submitButton: {
+    background: 'linear-gradient(45deg, #6190E8, #A7BFE8)',
+    color: 'white',
+    width: '100%',
+    marginTop: '10px',
+  },
+  background: {
+    justifyContent: "center",
+    minHeight: "30vh",
+    padding: "50px"
+  }
+});
+
+export default postStyles;
