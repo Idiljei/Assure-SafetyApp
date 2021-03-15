2e45992e335063c2b4fcce215ebc71a795345a91
Jinny Jang <jangjinny21@gmail.com>
2021-03-15 06:16:08 +0000

    update profile picture

--- react-front-end/src/components/Profile.js
+++ react-front-end/src/components/Profile.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
 import ContactsTwoToneIcon from '@material-ui/icons/ContactsTwoTone';
 import './profileStyles.css'
 
 const Profile = () => {
   return (
-    <div>
-      <PersonPinIcon>PersonPinIcon</PersonPinIcon>
+    <div class="profile-box">
+      <div class="profile-img">
+        <img src="pheebs.png" alt="profile"/>
+      </div>
+      
       <section class="profile-container">
-      <p>Name: Pheobe Buffay</p>
-      <p>Phone: 555-5555</p>
-      <p>D.O.B:</p>
+        <p>Name: Pheobe Buffay</p>
+        <p>Phone: 555-5555</p>
+        <p>D.O.B:</p>
       </section>
 
-    <section profile-container>
-      <h1 class= "profile-container"> Safety Network</h1>
-    </section>
+      <section profile-container>
+        <h1 class= "profile-container"> Safety Network</h1>
+      </section>
 
-    <section class="profile-container"> 
-      <p class="contacts">
-        <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Mom <br></br>123-456-7890  
-      </p>
-      <p class="contacts">
-        <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Sister <br></br> 345-678-9012
-      </p>
-      <p class="contacts">
-        <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Boothang <br></br> 234-567-8901
-      </p>
+      <section class="profile-container"> 
+        <p class="contacts">
+          <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Mom <br></br>123-456-7890  
+        </p>
+        <p class="contacts">
+          <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Sister <br></br> 345-678-9012
+        </p>
+        <p class="contacts">
+          <ContactsTwoToneIcon>ContactsTwoToneIcon</ContactsTwoToneIcon> Boothang <br></br> 234-567-8901
+        </p>
       </section>
     </div>
   );
