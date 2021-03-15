f78ad37571e3325e740d83e479f75c96d35d2e3d
Jinny Jang <jangjinny21@gmail.com>
2021-03-15 22:29:43 +0000

    working post request

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -47,13 +47,11 @@ App.get("/forum", async (req, res) => {
 // POST => create a post
 App.post("/forum/new", async(req, res) => {
   try {
-    console.log("Post Request")
     console.log(req.body)
-    // console.log("Post Request Inside Try")
-    const { user_id, title, address, description } = req.body;
+    const { user_id, title, address, description, created_at } = req.body;
+    const postData = [user_id, title, address, description, created_at];
     const newForum = await pool.query(
-      "INSERT INTO posts (user_id, title, address, description) VALUES ($1, $2, $3, $4)",
-      [user_id, title, address, description]
+      "INSERT INTO posts (user_id, title, address, description, created_at) VALUES ($1, $2, $3, $4, $5)", postData
     );
   } catch (err) {
     console.log(err);
