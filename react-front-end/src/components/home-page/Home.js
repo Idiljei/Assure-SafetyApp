32fce7742b5e486299a9d07c943d14dd8300cad6
Jinny Jang <jangjinny21@gmail.com>
2021-03-16 20:52:48 +0000

    create post fetch for sending texts

--- react-front-end/src/components/home-page/Home.js
+++ react-front-end/src/components/home-page/Home.js
@@ -7,12 +7,27 @@ import useStyles from '../Styles';
 const Home = (props) => {
   const classes = useStyles();
 
+  const sendSMS = async(e) => {
+    e.preventDefault();
+
+    const body = { "message": "Phoebe has sent her live location through Assure" }
+    await fetch('/sms', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+    .then(res =>  res.json())
+    .catch(err => console.log(err))
+  }
+
   return (
     <Box className={classes.homeBox}>
-      <Button className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
+      <Button onClick={sendSMS} className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
         Share Live Location
       </Button>
-      
+    
       <Button className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
         Call 911
       </Button>
