aba79b6a9c574c529574acbfe65c6063c6895a6f
Jinny Jang <jangjinny21@gmail.com>
2021-03-16 22:35:48 +0000

    update Search component

--- react-front-end/src/components/map-page/Search.js
+++ react-front-end/src/components/map-page/Search.js
@@ -29,7 +29,7 @@ const Search = ( props ) => {
       try {
         const results =  await getGeocode({ address });
         const { lat, lng } = await getLatLng(results[0])
-        console.log( lat, lng )   
+        panTo({ lat, lng })  
       } catch(error) {
       }
       console.log(address);
