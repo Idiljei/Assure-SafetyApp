87305bb4fdbe351e5b4333f838a8e9486089cc75
Idiljei <i.jeilani@gmail.com>
2021-03-17 19:31:24 +0000

    code cleanup

--- react-front-end/src/components/home-page/Home.js
+++ react-front-end/src/components/home-page/Home.js
@@ -3,32 +3,19 @@ import { Box, Button } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import ErrorIcon from '@material-ui/icons/Error';
 import useStyles from '../Styles';
+import { smsLocation } from './sms'
+import { smsPolice } from './sms'
 
 const Home = (props) => {
   const classes = useStyles();
 
-  const sendSMS = async(e) => {
-    e.preventDefault();
-
-    const body = { "message": "Phoebe has sent her live location through Assure" }
-    await fetch('/sms', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body),
-    })
-    .then(res =>  res.json())
-    .catch(err => console.log(err))
-  }
-
   return (
     <Box className={classes.homeBox}>
-      <Button onClick={sendSMS} className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
+      <Button onClick={smsLocation} className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
         Share Live Location
       </Button>
     
-      <Button className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
+      <Button onClick={smsPolice} className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
         Call 911
       </Button>
     </Box>
