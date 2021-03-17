75491ce207b21c59f91b8b4fdd36651b4240b5a7
Idiljei <i.jeilani@gmail.com>
2021-03-17 19:29:38 +0000

    twilio sms

---
+++ express-back-end/api/twilio.js
@@ -0,0 +1,19 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+
+client.messages
+  .create({
+    body: 'Monica is sharing her location with you',
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: process.env.USER_PHONE_NUM
+  })
+  .then(message => console.log(message.sid))
+  .catch((error) => console.log(error));
+
