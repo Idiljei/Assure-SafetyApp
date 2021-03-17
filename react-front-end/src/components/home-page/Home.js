79bb7d3421f4ec67c87fcf86ff38ed08efa675ee
Idiljei <i.jeilani@gmail.com>
2021-03-17 22:15:33 +0000

    added alerts to buttons

--- react-front-end/src/components/home-page/Home.js
+++ react-front-end/src/components/home-page/Home.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import ErrorIcon from '@material-ui/icons/Error';
+import Alert from '@material-ui/lab/Alert';
 import useStyles from '../Styles';
 import { smsLocation } from './sms'
 import { smsPolice } from './sms'
 
 const Home = (props) => {
   const classes = useStyles();
+  const [ selectLocation, setSelectedLocation ] = useState(false);
+  const [ selectPolicecall, setSelectPolicecall ] = useState(false);
+
+
+  const handleLocationClick = () => {
+    setSelectedLocation(true)
+    smsLocation()
+  }
+
+  const handlePoliceClick = () => {
+    setSelectPolicecall(true)
+    smsLocation()
+  }
 
   return (
     <Box className={classes.homeBox}>
-      <Button onClick={smsLocation} className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
+ {selectLocation ? 
+   <div>
+      <Button onClick={handleLocationClick} type="submit" className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
         Share Live Location
+      
       </Button>
-    
-      <Button onClick={smsPolice} className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
+      <Alert variant="outlined" severity="info">
+         sharing live location with safety network! 
+      </Alert>  
+   </div> : <Button onClick={handleLocationClick} type="submit" className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
+        Share Live Location
+      </Button> } 
+
+{selectPolicecall ? 
+  <div>
+      <Button />
+      <Alert variant="outlined" severity="success">
+        Calling Emergency Services 
+      </Alert>
+      <Button onClick={handlePoliceClick} className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
         Call 911
       </Button>
-    </Box>
+  </div> : <Button onClick={handlePoliceClick} className={classes.homeButton} size="large" startIcon={<ErrorIcon />} variant="contained">
+        Call 911
+      </Button>
+};
+ </Box>
+
   );
+
 };
 
 export default Home;
