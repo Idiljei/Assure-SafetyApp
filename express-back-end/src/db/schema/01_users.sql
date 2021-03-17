0573f22b621de240a1e3a2f45fb6500ae609872d
MaggieDeVito <maggiemaydevito@gmail.com>
2021-03-17 19:28:14 +0000

    reseeded database

--- express-back-end/src/db/schema/01_users.sql
+++ express-back-end/src/db/schema/01_users.sql
@@ -4,11 +4,10 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR (255) NOT NULL,
-  phone_number CHAR(10) NOT NULL,
-  date_of_birth DATE NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  pin VARCHAR(4) NOT NULL
-);
+  phone_number VARCHAR(13) NOT NULL,
+  date_of_birth VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+)
 
-GRANT ALL PRIVILEGES ON TABLE users TO spice;
-grant all on sequence posts_id_seq to spice;
+-- GRANT ALL PRIVILEGES ON TABLE users TO spice;
+-- grant all on sequence posts_id_seq to spice;
