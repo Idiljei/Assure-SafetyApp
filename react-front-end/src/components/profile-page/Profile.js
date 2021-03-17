d660fd07e1608687ab86b1dbd2da3a306df62596
MaggieDeVito <maggiemaydevito@gmail.com>
2021-03-17 21:06:05 +0000

    update profile page to show users safety contacts

--- react-front-end/src/components/profile-page/Profile.js
+++ react-front-end/src/components/profile-page/Profile.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { IconButton, Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import SimpleTabs from './profile-tabs/ProfileTab';
-import EditIcon from '@material-ui/icons/Edit';
-import './profileStyles.css'
+import React, { useState, useEffect } from "react";
+import { IconButton, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import SimpleTabs from "./profile-tabs/ProfileTab";
+import EditIcon from "@material-ui/icons/Edit";
+import "./profileStyles.css";
 
 const profileStyles = makeStyles({
   root: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: '100%'
+    width: "100%",
   },
   icon: {
-    color: '#f7f7f7'
-  }
+    color: "#f7f7f7",
+  },
 });
 
 const Profile = () => {
   const classes = profileStyles();
   const [users, setUser] = useState([]);
-  const [ selected, setSelected ] = useState(false);
+  const [selected, setSelected] = useState(false);
 
   const toggle = () => {
     if (selected) return setSelected(false);
     if (!selected) return setSelected(true);
-  }
+  };
 
   const getUser = async () => {
     try {
       const id = 3;
-      const response = await fetch(`http://localhost:8080/${id}`);
+      const response = await fetch(`http://localhost:8080/user/${id}`);
       const jsonData = await response.json();
 
       setUser(jsonData);
@@ -43,27 +43,47 @@ const Profile = () => {
     getUser();
   }, []);
 
-return (
-  <div class="profile">
-  { users.map(user => {
-    return (
-      <Box display="flex" height="100%" width="100%" justifyContent="center" alignItems="center" flexDirection="column">
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <img src="pheebs.png" alt="profile" class="profile-img"/>
-            <Box display="flex" alignItems="center">
-              <h1 class="user-name"> {user.first_name} {user.last_name}</h1>
-              <IconButton aria-label="edit" onClick={toggle}>
-                <EditIcon className={classes.icon}/>
-              </IconButton>
+  return (
+    <div class="profile">
+      {users.map((user) => {
+        return (
+          <Box
+            display="flex"
+            height="100%"
+            width="100%"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <img src="pheebs.png" alt="profile" class="profile-img" />
+              <Box display="flex" alignItems="center">
+                <h1 class="user-name">
+                  {" "}
+                  {user.first_name} {user.last_name}
+                </h1>
+                <IconButton aria-label="edit" onClick={toggle}>
+                  <EditIcon className={classes.icon} />
+                </IconButton>
+              </Box>
             </Box>
-        </Box>
-        <Box>
-          <SimpleTabs number={user.phone_number} dob={user.date_of_birth} selected={selected}/> 
-        </Box>
-      </Box>
-    )})}
+            <Box>
+              <SimpleTabs
+                number={user.phone_number}
+                dob={user.date_of_birth}
+                selected={selected}
+              />
+            </Box>
+          </Box>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default Profile;
--- react-front-end/src/components/profile-page/profile-tabs/ContactList.js
+++ react-front-end/src/components/profile-page/profile-tabs/ContactList.js
@@ -1,57 +1,91 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { List, ListItem, ListItemIcon, ListItemText, Divider, Box } from '@material-ui/core';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import DeleteButton from '../buttons/DeleteButton';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Box,
+} from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import DeleteButton from "../buttons/DeleteButton";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 500,
-    backgroundColor: 'transparent',
-    color: 'white'
+    backgroundColor: "transparent",
+    color: "white",
   },
   icon: {
-    color: 'white'
-  }
+    color: "white",
+  },
 }));
 
 const contacts = [
   {
     name: "Mom",
-    number: "123-456-7890"
+    number: "123-456-7890",
   },
   {
     name: "Sister",
-    number: "123-456-7890"
+    number: "123-456-7890",
   },
   {
     name: "Dad",
-    number: "123-456-7890"
-  }
+    number: "123-456-7890",
+  },
 ];
 
 export default function ContactList(props) {
   const classes = useStyles();
   const selected = props.selected;
+  const [networks, setNetwork] = useState([]);
+
+  const getNetwork = async () => {
+    try {
+      const id = 3;
+      const response = await fetch(`http://localhost:8080/safetynetwork/${id}`);
+      const jsonData = await response.json();
+
+      setNetwork(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getNetwork();
+  }, []);
 
   return (
     <div className={classes.root}>
-      { contacts.map(contact => {
+      {networks.map((network) => {
         return (
           <div>
-            <List component="nav" aria-label={contact.name}>
-              <ListItem button >
+            <List
+              component="nav"
+              aria-label={network.first_name + network.last_name}
+            >
+              <ListItem button>
                 <ListItemIcon>
-                  <AccountCircleIcon className={classes.icon} fontSize="large"/>
+                  <AccountCircleIcon
+                    className={classes.icon}
+                    fontSize="large"
+                  />
                 </ListItemIcon>
-                <ListItemText className={classes.icon} primary={contact.name} secondary={contact.number} />
-                { selected ? <DeleteButton /> : null}
+                <ListItemText
+                  className={classes.icon}
+                  primary={network.first_name + " " + network.last_name}
+                  secondary={network.phone_number}
+                />
+                {selected ? <DeleteButton /> : null}
               </ListItem>
             </List>
             <Divider />
           </div>
-        )
+        );
       })}
     </div>
   );
