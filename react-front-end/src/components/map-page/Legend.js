387b2d143aaab06637692306d06bfb4b27c391fe
Jinny Jang <jangjinny21@gmail.com>
2021-03-17 07:18:25 +0000

    create legend for map

---
+++ react-front-end/src/components/map-page/Legend.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import './search.css';
+
+const Legend = () => {
+
+  return (
+    <Box display="flex">
+      <Box display="flex" justifyContent="center" alignItems="center" marginRight="1em">
+        <img src="./report.svg" alt="report icon" class="report-icon"/>
+        <h4 class="legend">
+          Reported Crimes
+        </h4>
+      </Box>
+
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img src="./hospital.svg" alt="hospital icon" class="report-icon" />
+        <h4 class="legend">
+          Hospitals
+        </h4>
+      </Box>
+
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img src="./police.svg" alt="police icon" class="report-icon" />
+        <h4 class="legend">
+          Police Stations
+        </h4>
+      </Box>
+
+    </Box>
+  )
+};
+
+export default Legend;
