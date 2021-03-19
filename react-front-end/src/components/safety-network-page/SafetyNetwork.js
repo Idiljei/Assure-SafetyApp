856a5f635970fa8d8c23acec8bc18a4a114c7ed4
Jinny Jang <jangjinny21@gmail.com>
2021-03-19 20:46:51 +0000

    add user markers and add photos

--- react-front-end/src/components/safety-network-page/SafetyNetwork.js
+++ react-front-end/src/components/safety-network-page/SafetyNetwork.js
@@ -1,13 +1,13 @@
-import React, { useCallback, useRef } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
 import { Box } from "@material-ui/core";
 import Locate from "../map-page/Locate";
-import Legend from "../map-page/Legend";
 import mapStyles from "../map-page/mapStyles";
 import useStyles from "../Styles";
 import MapSearch from "../map-page/MapSearch";
 import "@reach/combobox/styles.css";
 import "../map-page/search.css";
+import { parse } from "date-fns";
 
 const containerStyle = {
   width: "100%",
@@ -29,6 +29,37 @@ const options = {
 
 const SafetyNetworkMap = () => {
   const classes = useStyles();
+  const [ userSn, setUserSn ] = useState([]);
+  const [ selected, setSelected ] = useState(null);
+
+  const getUserNetwork = async () => {
+    try {
+      const id = 3;
+      const response = await fetch(`http://localhost:8080/snlocation/${id}`);
+      const jsonData = await response.json();
+      
+    jsonData.map(data => {
+
+      const parsed = JSON.parse(data.current_location)
+
+      const userInfo = {
+        name: data.first_name, 
+        number: data.phone_number,
+        lat: parsed.lat,
+        lng: parsed.lng
+      }
+      
+      return setUserSn((prev) => [...prev, userInfo])
+    })
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserNetwork();
+  }, []);
 
   const { loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
@@ -48,6 +79,7 @@ const SafetyNetworkMap = () => {
 
   if (loadError) return "Error loading maps";
 
+
   return (
     <div className="map">
     <Box display="flex" flexDirection="column">
@@ -65,10 +97,32 @@ const SafetyNetworkMap = () => {
         onLoad={onMapLoad}
         >
 
+      { userSn.map((sn) => {
+          return(
+          <Marker 
+            position={{ lat: sn.lat, lng: sn.lng }}
+            onClick={() => {
+              setSelected(sn);
+            }}
+            /> )
+        })}
+
+      { selected ? (
+        <InfoWindow
+          position={{ lat: selected.lat, lng: selected.lng }}
+          onCloseClick={() => {
+            setSelected(null)
+          }}
+        >
+          <div>
+            <h2>{ selected.name }</h2>
+            <h2>{ selected.number }</h2>
+          </div>
+        </InfoWindow>
+      ) : null } 
+
       </GoogleMap>
       </Box>
-
-      <Legend />
 
       <Box className={classes.contacts}>
       </Box>
