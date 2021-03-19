5b8636cbf4e1d551da336adef2b16ebf3041b152
Jinny Jang <jangjinny21@gmail.com>
2021-03-19 02:53:13 +0000

    create button to share location

---
+++ react-front-end/src/components/home-page/alert/LocationButton.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Button, Box } from '@material-ui/core';
+import ConfirmCancel from '../alert/ConfirmCancel';
+import SendIcon from '@material-ui/icons/Send';
+import useStyles from '../../Styles';
+
+const LocationButton = (props) => {
+  const classes = useStyles();
+
+  const option = props.location;
+  const setOption = props.setLocation;
+
+  const status = {
+    before: "Share Live Location",
+    after: "Currently sharing your Live Location"
+  }
+
+  const handleClick = () => {
+    if (!option) {
+      setOption(1)
+      // smsLocation()
+    }
+
+    if (option === 1) {
+      setOption(2)
+    }
+  }
+
+  console.log("This is the option: ", option)
+
+  return (
+    <Box className={classes.home}>
+      <Button onClick={handleClick} type="submit" className={classes.homeButton} size="large" startIcon={<SendIcon />} variant="contained">
+        
+        { !option && status.before }
+        { option === 1 && status.after }
+
+      </Button>
+
+      { option > 1 ? <ConfirmCancel id="location" option={option} setOption={setOption} /> : null }
+
+    </Box>
+  )
+};
+
+export default LocationButton;
