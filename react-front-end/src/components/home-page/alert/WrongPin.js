3765c679dcc0b16d1541520e22b91460dfada2f1
Jinny Jang <jangjinny21@gmail.com>
2021-03-19 02:52:54 +0000

    feedback for incorrect pin

---
+++ react-front-end/src/components/home-page/alert/WrongPin.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Paper, Dialog, DialogActions, DialogContent, DialogContentText, Button, DialogTitle } from '@material-ui/core';
+
+const WrongPin = (props) => {
+  const option = props.option;
+  const setOption = props.setOption;
+  const id = props.id;
+
+  const handleClose = () => {
+    setOption(1)
+  }
+
+  const handleSubmit = () => {
+    setOption(2)
+  }
+
+  return (
+    <div>
+      <DialogTitle id="form-dialog-title">Incorrect PIN</DialogTitle>
+      <DialogContent>
+      
+      { id === 'location' && 
+        <DialogContentText id="alert-dialog-description">
+          Live Location is still being shared!
+        </DialogContentText> }
+
+      { id === 'police' &&
+        <DialogContentText id="alert-dialog-description">
+          Emergency Services is being contacted!
+        </DialogContentText> }
+
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Try Again
+        </Button>
+      </DialogActions>
+    </div>
+  )
+};
+
+export default WrongPin;
