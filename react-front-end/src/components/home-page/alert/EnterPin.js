7b56095121ab5931975dfe0a5ddfae55db54d6cb
Jinny Jang <jangjinny21@gmail.com>
2021-03-19 02:52:44 +0000

    area to enter pin

---
+++ react-front-end/src/components/home-page/alert/EnterPin.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Paper, Dialog, DialogActions, DialogContent, TextField, Button, DialogTitle } from '@material-ui/core';
+
+const userPIN = 2021;
+const limit = 4;
+
+const EnterPin = (props) => {
+  const option = props.option;
+  const setOption = props.setOption;
+
+  const [ pin, setPin ] = useState(0);
+
+  const handleClose = () => {
+    setOption(1)
+  }
+
+  const handleSubmit = () => {
+    if (userPIN === parseInt(pin)) {
+      setOption(3);
+    } else {
+      setOption(4);
+    }
+  }
+
+  console.log("This is the option: ", option)
+  console.log(pin)
+
+  return (
+    <div>
+      <DialogTitle id="form-dialog-title">Enter Pin if Safe</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          inputProps={{
+            maxLength: limit
+          }}
+          type="password"
+          onChange={e => setPin(e.target.value)}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Enter
+        </Button>
+      </DialogActions>
+    </div>
+
+  )
+};
+
+export default EnterPin;
