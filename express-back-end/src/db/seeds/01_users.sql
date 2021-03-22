656cfb4115b1fea89e862e557e10b8f662e552e0
Idiljei <i.jeilani@gmail.com>
2021-03-22 02:43:56 +0000

    safetynetworkphotos

--- express-back-end/src/db/seeds/01_users.sql
+++ express-back-end/src/db/seeds/01_users.sql
@@ -2,7 +2,7 @@
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Cece', 'Parekh', '205-365-1800', 'May 5, 1985', 'cece@gmail.com', '{"lat":49.1244123, "lng":-123.054313}', '/avatars/cece.jpg');
+  ('Cece', 'Parekh', '205-365-1800', 'May 5, 1985', 'cece@gmail.com', '{"lat":49.1244123, "lng":-123.054313}', '/avatars/cece.png');
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
@@ -10,30 +10,30 @@ VALUES
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Jessica', 'Day', '144-387-1970', 'Feb 16, 1985', 'jessica@gmail.com', '{"lat":49.123324, "lng":-123.123413}', '/avatars/jessica.jpg');
+  ('Jessica', 'Day', '144-387-1970', 'Feb 16, 1985', 'jessica@gmail.com', '{"lat":49.123324, "lng":-123.123413}', '/avatars/jessica.png');
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img, sharing_location)
 VALUES
-  ('Schmidt', 'W', '342-387-1560', 'Apr 8, 1985', 'schmidt@gmail.com', '{"lat":49.52624, "lng":-123.023413}', '/avatars/schmidt.jpg', true);
+  ('Schmidt', 'W', '342-387-1560', 'Apr 8, 1985', 'schmidt@gmail.com', '{"lat":49.52624, "lng":-123.023413}', '/avatars/schmidt.png', true);
 
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Wintson','Bishop','123-349-1160','Jan 9, 1985','wintsonb@gmail.com', '{"lat":49.2311957,"lng":-123.1336806}',  '/avatars/wintson.jpg');
+  ('Wintson','Bishop','123-349-1160','Jan 9, 1985','wintsonb@gmail.com', '{"lat":49.2311957,"lng":-123.1336806}',  '/avatars/wintson.png');
 
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img, sharing_location)
 VALUES
-  ('Coach', 'W', '285-573-5739', 'Oct 18, 1986', 'coach@gmail.com', '{"lat":49.2811954,"lng":-123.1326803}',  '/avatars/coach.jpg', true);
+  ('Coach', 'W', '285-573-5739', 'Oct 18, 1986', 'coach@gmail.com', '{"lat":49.2811954,"lng":-123.1326803}',  '/avatars/coach.png', true);
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Aly', 'Nelson', '285-573-5739', 'Oct 18, 1985', 'aly@gmail.com', '{"lat":49.4324667, "lng":-123.2013215}',  '/avatars/aly.jpg');
+  ('Aly', 'Nelson', '285-573-5739', 'Oct 18, 1985', 'aly@gmail.com', '{"lat":49.4324667, "lng":-123.2013215}',  '/avatars/aly.png');
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Joan', 'Day', '144-387-1970', 'Feb 16, 1960', 'joan@gmail.com', '{"lat":49.2811956,"lng":-123.1306807}', '/avatars/joan.jpg');
+  ('Joan', 'Day', '144-387-1970', 'Feb 16, 1960', 'joan@gmail.com', '{"lat":49.2811956,"lng":-123.1306807}', '/avatars/joan.png');
 INSERT INTO users
   (first_name, last_name, phone_number, date_of_birth, email, current_location, img)
 VALUES
-  ('Alisha', 'Bishop', '144-456-1290', 'Feb 16, 1987', 'alisha@gmail.com', '{"lat":49.2811956,"lng":-123.1306807}', '/avatars/alisha.jpg');
+  ('Alisha', 'Bishop', '144-456-1290', 'Feb 16, 1987', 'alisha@gmail.com', '{"lat":49.2811956,"lng":-123.1306807}', '/avatars/alisha.png');
