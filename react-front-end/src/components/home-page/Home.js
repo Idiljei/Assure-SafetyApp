1ec6a3dda0b82c98f01a19e244ec9456e928868c
MaggieDeVito <maggiemaydevito@gmail.com>
2021-03-24 05:39:57 +0000

    fixed more conflicts

--- react-front-end/src/components/home-page/Home.js
+++ react-front-end/src/components/home-page/Home.js
@@ -5,11 +5,6 @@ import EmergencyButton from "./alert/EmergencyButton";
 import useStyles from "../Styles";
 import PlayPause from "../home-page/alert/Siren";
 import "././alert/Alertpopup.css";
-<<<<<<< HEAD
-=======
-import SurroundSoundOutlinedIcon from "@material-ui/icons/SurroundSoundOutlined";
-import { IconButton, Button } from "@material-ui/core";
->>>>>>> styles/profile-posts
 
 const Home = () => {
   const classes = useStyles();
@@ -38,30 +33,36 @@ const Home = () => {
   return (
     <div>
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <img class="logo-picture" src="logo/assure-logo.png" alt="assure logo" />
+        <img
+          class="logo-picture"
+          src="logo/assure-logo.png"
+          alt="assure logo"
+        />
         <div class="alarm">
-        <PlayPause></PlayPause>
-      </div>
-      <div>
-        <Box className={classes.homeBox}>
-          <Box margin="2em">
-            <LocationButton
-              id="location"
-              userStatus={userStatus}
-              setUserStatus={setUserStatus}
-              checkLocationStatus={checkLocationStatus}
-            />
-          </Box>
-          <Box margin="2em">
-            <EmergencyButton
-              id="police"
-              policeStatus={policeStatus}
-              setPoliceStatus={setPoliceStatus}
-            />
-          </Box>
-          <Box></Box>
+          <PlayPause></PlayPause>
+        </div>
+      </Box>
+
+      <Box className={classes.homeBox}>
+        <Box margin="2em">
+          <LocationButton
+            id="location"
+            userStatus={userStatus}
+            setUserStatus={setUserStatus}
+            checkLocationStatus={checkLocationStatus}
+          />
         </Box>
-      </div>
+
+        <Box margin="2em">
+          <EmergencyButton
+            id="police"
+            policeStatus={policeStatus}
+            setPoliceStatus={setPoliceStatus}
+          />
+        </Box>
+
+        <Box></Box>
+      </Box>
     </div>
   );
 };
